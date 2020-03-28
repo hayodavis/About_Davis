b7a5087232283df01da0f12d39abf32caae76eb4
Davis <davis.fandi@gmail.com>
2020-03-28 20:04:00 +0700

    Update code using DataBinding for code is shorter, easier to read, and easier to maintain than code uses findByView()

--- app/src/main/java/com/davis/aboutdavis/MainActivity.kt
+++ app/src/main/java/com/davis/aboutdavis/MainActivity.kt
@@ -5,40 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.davis.aboutdavis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val myName: MyName = MyName("Davis RIfandi")
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // call funtion addnickname with click button
-        findViewById<Button>(R.id.done_button).setOnClickListener{addNickname(it)}
+        binding.doneButton.setOnClickListener{addNickname(it)}
 
         // call function update nickname whit click textview
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener{updateNickname(it)}
+        binding.nicknameText.setOnClickListener { updateNickname(it) }
+
+        // access variabel my name with object
+        binding.myName = myName
     }
 
     // funtion for add nickname to textview
     private fun addNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        // add value textview from edittext
-        nickNameTextView.text = editText.text
-
-        // hidden edit text
-        editText.visibility = View.GONE
-
-        // hidden button
-        view.visibility = View.GONE
-
-        // visible text view
-        nickNameTextView.visibility = View.VISIBLE
-
+        // apply binding to reduce the use findviewbyid
+        binding.apply {
+            // add value textview from edittext
+            myName?.nickname = nicknameEdit.text.toString()
+            // UI refresh update
+            invalidateAll()
+            // hidden edit text
+            nicknameEdit.visibility = View.GONE
+            // hidden button
+            doneButton.visibility = View.GONE
+            // visible text view
+            nicknameText.visibility = View.VISIBLE
+        }
         // hidden keyboard
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
@@ -46,21 +50,19 @@ class MainActivity : AppCompatActivity() {
 
     // function for update nickname
     private fun updateNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.done_button)
 
-        // visible edittext
-        editText.visibility = View.VISIBLE
-
-        // visible button
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.VISIBLE
-
-        // set focus to edittext
-        editText.requestFocus()
-
+        // apply binding to reduce the use findviewbyid
+        binding.apply {
+            // visible edittext
+            nicknameEdit.visibility = View.VISIBLE
+            // visible button
+            doneButton.visibility = View.VISIBLE
+            view.visibility = View.VISIBLE
+            // set focus to textview
+            nicknameText.requestFocus()
+        }
         // show keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 }
---
+++ app/src/main/java/com/davis/aboutdavis/MyName.kt
@@ -0,0 +1,5 @@
+package com.davis.aboutdavis
+
+data class MyName(var name: String = "", var nickname: String = "") {
+
+}
